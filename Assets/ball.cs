0ef2d810de363c563eda98ea92b8c00d3f3b4862
Kanon159 <c0114159d1@edu.teu.ac.jp>
2017-03-07 09:53:28 +0900

    first commit

---
+++ Assets/ball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+	//　初期スピード
+	private float speed = 20.0f;
+
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody>().AddForce(
+			(transform.forward + transform.right) * speed,
+			ForceMode.VelocityChange);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	private float accel = 1000.0f;
+	private Vector3 inputVector;
+	private Rigidbody rig;
+
+	// Use this for initialization
+	void Start () {
+		rig = this.GetComponent<Rigidbody>();
+		inputVector = new Vector3(0.0f, 0.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		//力を加える
+//		this.GetComponent<Rigidbody> ().AddForce (
+//			transform.right * Input.GetAxisRaw ("Horizontal") * accel, 
+//			ForceMode.Impulse);
+		inputVector = transform.right * Input.GetAxisRaw("Horizontal") * accel;
+	}
+
+	void FixedUpdate(){
+		//力を加える
+		rig.AddForce(inputVector, ForceMode.Impulse);
+	}
+}
